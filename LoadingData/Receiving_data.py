29e8c9bab65808079de0fefa74a89e446cc46979
asher zeev aush <A0583251203@gmail.com>
2025-09-07 12:50:18 +0300

    A class outputs a gson with the path and metadata of the file.

---
+++ LoadingData/Receiving_data.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+import json
+import os
+# my_path = Path("C:\podcasts")
+
+
+class send_info_data:
+    def __init__(self, path):
+        self.path = path
+        self.files = [x for x in self.path.iterdir() if x.is_file()]
+
+    def get_data(self):
+        data = []
+        for file in self.files:
+            file_info = {
+                "full_path": str(file.resolve()),
+                "Name suffix": file.name,
+                "name": file.stem,
+                "file type": file.suffix,
+                "size_MB": file.stat().st_size / (1024**2),
+                "creation_time": file.stat().st_ctime,
+                "is_file" : file.is_file()
+
+            }
+            data.append(file_info)
+        return json.dumps(data, indent=4)
+
+# if __name__ == "__main__":
+#     sender = send_info_data(my_path)
+#     data = sender.get_data()
+#     print(data)
+
